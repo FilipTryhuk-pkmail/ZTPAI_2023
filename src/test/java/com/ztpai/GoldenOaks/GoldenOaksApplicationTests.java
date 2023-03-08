828c45905a5120f78b61927f39c9bf1f014937ba
Filip Tryhuk <filip.tryhuk@student.pk.edu.pl>
2023-03-08 23:23:15 +0100

    initial commit

---
+++ src/test/java/com/ztpai/GoldenOaks/GoldenOaksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ztpai.GoldenOaks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenOaksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
