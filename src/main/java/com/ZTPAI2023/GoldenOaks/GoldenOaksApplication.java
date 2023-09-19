0e8832177ba07a0b4bd54266a903d67ea7d7cb04
Filip Tryhuk <filip.tryhuk@student.pk.edu.pl>
2023-09-19 04:22:54 +0200

    JWT authentication

--- src/main/java/com/ZTPAI2023/GoldenOaks/GoldenOaksApplication.java
+++ src/main/java/com/ZTPAI2023/GoldenOaks/GoldenOaksApplication.java
@@ -1,8 +1,11 @@
 package com.ZTPAI2023.GoldenOaks;
 
+import com.ZTPAI2023.GoldenOaks.security.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class GoldenOaksApplication {
 
